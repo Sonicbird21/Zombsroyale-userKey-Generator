a493445d7590957d99b00704cb8f84ac6dcf930f
SonicZR <67420120+SonicZR@users.noreply.github.com>
2023-02-21 18:22:42 +0100

    version 2.0

--- userKeyGen.py
+++ userKeyGen.py
@@ -1,32 +1,39 @@
 import random
 import requests
+import sys
+import threading
 
 print("-" * 50)
-print("Sonic's Zombsroyale UserKeyGen v.1.0")
+print("Sonic's Zombsroyale UserKeyGen v.2.0")
 print("Press Enter to start")
 print("-" * 50)
 input("")
 
+def check_key(userKey):
+    response = requests.head(f"https://zombsroyale.io/user/{userKey}")
+    if response.status_code == 200:
+        print(f"Success! Key found: {userKey}")
+        with open("foundUserkeys.txt", "a") as o:
+            o.write(userKey + "\n")
+    elif response.status_code == 401:
+        print(f"Key not found: {userKey}")
+
 def gen():
     try:
         while True:
-            hash = random.getrandbits(192)
-            userKey = "%032x" % hash
-            response = requests.head('https://zombsroyale.io/user/{userKey}')
-            print(userKey)
+            threads = []
+            for i in range(10):
+                hash = random.getrandbits(192)
+                userKey = "%048x" % hash
+                thread = threading.Thread(target=check_key, args=(userKey,))
+                threads.append(thread)
+                thread.start()
+            
+            for thread in threads:
+                thread.join()
 
-            if response.status_code == 200:
-                print('Success!')
-                with open("foundUserkeys.txt", "a") as o:
-                    o.write(userKey)
-
-            elif response.status_code == 401:
-                    print('Not Found')
-            else:
-                print("Connection timed out! (Probably blocked)")
     except KeyboardInterrupt:
         print("\nExiting program.")
-        exit()
-
+        sys.exit()
 
 gen()
